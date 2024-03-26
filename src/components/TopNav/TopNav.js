b4c94f826d3b9563c23f13d7f19854e1fcf01b12
ashfaq11 <ashfaqonly1@gmail.com>
2024-03-26 16:38:23 +0000

    alignment

--- src/components/TopNav/TopNav.js
+++ src/components/TopNav/TopNav.js
@@ -6,6 +6,27 @@ import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom';
 //import {GoogleLogin} from "react-google-login"
 //import {gapi} from 'gapi-script';
+import { useMediaQuery } from 'react-responsive'
+{/*}
+const Example = () => {
+  const isDesktopOrLaptop = useMediaQuery({
+    query: '(min-width: 1224px)'
+  })
+  const isBigScreen = useMediaQuery({ query: '(min-width: 1824px)' })
+  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
+  const isPortrait = useMediaQuery({ query: '(orientation: portrait)' })
+  const isRetina = useMediaQuery({ query: '(min-resolution: 2dppx)' })
+  return (
+    <div>
+      <h1>Device Test!</h1>
+      {isDesktopOrLaptop && <p>You are a desktop or laptop</p>}
+      {isBigScreen && <p>You have a huge screen</p>}
+      {isTabletOrMobile && <p>You are a tablet or mobile phone</p>}
+      <p>Your are in {isPortrait ? 'portrait' : 'landscape'} orientation</p>
+      {isRetina && <p>You are retina</p>}
+    </div>
+  )
+}*/}
 const TopNav = function()
 {
 
@@ -20,7 +41,13 @@ const TopNav = function()
         <div className='header bg-dark'>
         <div className='topnavrow row'>
           <div className='brand my-1'>
-            <h1> Coforge Rewards Shop </h1>
+            <h1 style={{textAlign: "center"}}> Coforge Rewards Shop </h1>
+            <div className='cart-wishlist'>
+            <ul className="p-0">
+               <li className="list-icon" > <Link to="/cart"> <FontAwesomeIcon icon="fa fa-shopping-cart" style={{color:"white"}}/>{cartItemCount>0 && <div className='cart-item-count'><p>{cartItemCount}</p></div>}</Link> </li>
+            </ul>
+
+          </div>
           </div>
          {/*
          
@@ -42,15 +69,7 @@ const TopNav = function()
               </h5>
           </div>
           */}
-          <div className='cart-wishlist'>
-            <ul className="p-0">
- 
-           
-           <li className="list-icon" > <Link to="/cart"> <FontAwesomeIcon icon="fa fa-shopping-cart" style={{color:"white"}}/>{cartItemCount>0 && <div className='cart-item-count'><p>{cartItemCount}</p></div>}</Link> </li>
          
-            </ul>
-
-          </div>
         
         </div>
       </div>
